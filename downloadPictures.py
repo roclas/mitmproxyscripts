bb912a01f18749112d1af2bfbd6052de44c51821
Carlos <carlopolo@gmail.com>
2019-12-20 15:32:02 +0000

    fixed

--- downloadPictures.py
+++ downloadPictures.py
@@ -1,15 +1,23 @@
-import Image, cStringIO, os,re, urllib
+#mitmproxy -p 8000 -s logimages.py --view-filter "~s .*.png$"
+import os,re, urllib
+import urllib.request
+f= open("/tmp/pngs.txt","w+")
 
-def request(context, flow):
-  if( re.match(".*(\.gif|\.png|\.jpe?g)", flow.request.path,flags=re.IGNORECASE)):
-    	  #f = open('/home/carlos/borrar_log_mitmproxy.txt', 'wa')
-	  mydir="/".join(flow.request.path.split("/")[:-1])
-	  destdir=flow.request.host+mydir
-	  os.system("mkdir -p "+destdir)
-	  filename=flow.request.path.split("/")[-1]
-	  downloadfile=(destdir+"/"+filename).split("?")[0]
-	  downloadurl=downloadfile
-	  if(not downloadurl.startswith("http")):
-		downloadurl="http://"+downloadurl
-	  #f.write("downloading "+downloadfile)
-	  urllib.urlretrieve( downloadurl,downloadfile )
+def downloadfile(url,path):
+    myfile = open(path, 'wb')
+    response = urllib.request.urlopen(url)
+    myfile.write(response.read())
+    myfile.close()
+
+def response(flow):
+    if flow.response.headers.get("content-type", "").startswith("image"):
+        mydir="/tmp/proxied_files/".join(flow.request.path.split("/")[:-1])
+        destdir=mydir+flow.request.host
+        os.system("mkdir -p "+destdir)
+        filename=flow.request.path.split("/")[-1]
+        path=(destdir+"/"+filename).split("?")[0]
+        downloadurl=flow.request.url
+        if(not downloadurl.startswith("http")):
+            downloadurl="http:/"+downloadurl
+        f.write("%s -> %s\n"%(downloadurl,path))
+        downloadfile( downloadurl,path)
